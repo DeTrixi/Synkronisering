6f9f956e6804d1e76128c4df000554fddaa42e6c
Your Name <flemming@northtech.dk>
2020-12-15 07:46:30 +0100

    initial commit

---
+++ Opgave1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace Opgave1
+{
+    class Program
+    {
+        private static int count = 0;
+        private static object baton = new object();
+
+        static void Main(string[] args)
+        {
+            new Thread(CountUp).Start();
+
+            Thread.Sleep(500);
+            new Thread(CountDown).Start();
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void CountUp(object obj)
+        {
+            do
+            {
+                Monitor.Enter(baton);
+
+                try
+                {
+                    Console.WriteLine($"im in {Thread.CurrentThread.ManagedThreadId}");
+                    count += 2;
+                    Console.WriteLine(count);
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+                    Monitor.Exit(baton);
+                }
+            } while (true);
+        }
+
+        public static void CountDown()
+        {
+            do
+            {
+                Monitor.Enter(baton);
+
+                try
+                {
+                    Console.WriteLine($"im in {Thread.CurrentThread.ManagedThreadId}");
+                    count--;
+                    Console.WriteLine(count);
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+                    Monitor.Exit(baton);
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Opgave2og3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace Opgave2
+{
+    class Program
+    {
+        public static int Count;
+        public static object Baton = new object();
+
+        static void Main(string[] args)
+        {
+            Thread printStars = new Thread(PrintStars) {Name = "Stars"};
+            Thread printHash = new Thread(PrintHash) {Name = "Hash"};
+            printStars.Start();
+            printHash.Start();
+
+            Console.WriteLine("Hello Teacher!");
+            Console.ReadLine();
+        }
+
+        private static void PrintStars(object obj)
+        {
+            do
+            {
+                lock (Baton)
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write($"*");
+                        Count++;
+                        Thread.Sleep(10);
+                    }
+
+                    Console.WriteLine($" {Count}");
+                }
+            } while (true);
+        }
+
+        private static void PrintHash(object obj)
+        {
+            do
+            {
+                lock (Baton)
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write($"#");
+                        Count++;
+                        Thread.Sleep(10);
+                    }
+                    Console.WriteLine($" {Count}");
+                }
+
+
+
+            } while (true);
+        }
+    }
+}
